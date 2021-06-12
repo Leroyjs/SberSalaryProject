74ec18656d7dc2994e455a778183de920826f541
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-06-12 23:46:51 +0500

    Add figure to feature slider

--- src/js/particles/features.js
+++ src/js/particles/features.js
@@ -1,18 +1,64 @@
 import Flickity from 'flickity';
 import 'flickity/dist/flickity.min.css';
 
+const _sliderFeatures = document.querySelector('.slider-features');
+const _prevItem = document.querySelector('.slider-features__button_prev');
+const _nextItem = document.querySelector('.slider-features__button_next');
+const _pageDots = document.querySelector('.page-dots');
 
-const _sliderElement = document.querySelector('.slider-features');
+const imgs = _sliderFeatures.querySelectorAll('.slide-figure');
+const docStyle = document.documentElement.style;
+const transformProp = typeof docStyle.transform == 'string' ?
+    'transform' : 'WebkitTransform';
 
-const featuresSlider = new Flickity(_sliderElement, {
+const dotsArray = [];
+let activeDot = null;
+
+const featuresSlider = new Flickity(_sliderFeatures, {
     cellAlign: 'left',
     percentPosition: false,
     prevNextButtons: false,
+    wrapAround: true,
+    pageDots: false,
     on: {
         scroll: function () {
-            console.log(this.x);
+            const carousel = this;
+            const carouselWidth = carousel.size.width;
+            imgs.forEach(function (img) {
+                // const x = (slide.target + carousel.x) * -1 / 3;
+                const x = (carousel.x % carouselWidth) * (-1 / 6);
+                img.style[transformProp] = 'translateX(' + x + 'px)';
+            });
+        },
+        change: function (index) {
+            activeDot && activeDot.classList.remove('page-dots__dot_active');
+            activeDot = dotsArray[index];
+            activeDot.classList.add('page-dots__dot_active');
         }
     },
 });
+
+
+for (let i = 0; i < featuresSlider.slides.length; i++) {
+    const dotElement = document.createElement('li');
+    if (i == 0) {
+        dotElement.classList.add('page-dots__dot');
+        dotElement.classList.add('page-dots__dot_active');
+        activeDot = dotElement;
+    } else
+        dotElement.classList.add('page-dots__dot');
+
+    _pageDots.appendChild(dotElement);
+    dotsArray.push(dotElement);
+}
+
+_prevItem.addEventListener('click', () => {
+    featuresSlider.next();
+});
+
+
+_nextItem.addEventListener('click', () => {
+    featuresSlider.previous();
+})
 
 
