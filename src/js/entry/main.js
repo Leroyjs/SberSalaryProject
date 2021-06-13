3dcc2bce305804676e02a3d601189f367f984076
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-06-14 03:06:58 +0500

    Add paralax effect on scroll feature

--- src/js/entry/main.js
+++ src/js/entry/main.js
@@ -1,27 +1,28 @@
 import '~/sass/main';
-import { initPreloader } from '~/js/particles/preloader';
-import { initMainForm } from '~/js/particles/mainForm';
-import { chengeCompany } from '~/js/particles/chengeCompany';
-import { ancorsInit } from '~/js/particles/ancors';
-import { initAcquainted } from '~/js/particles/acquainted';
-import { getActiveCompanyByGetParameter } from '~/js/particles/getActiveCompanyByGetParameter';
-import { initFeatureSlider } from '~/js/particles/features';
-import { initPromoSlider } from '~/js/particles/promos';
-import { initVideoPlayers } from '~/js/particles/promos';
+import {initPreloader} from '~/js/particles/preloader';
+import {initMainForm} from '~/js/particles/mainForm';
+import {chengeCompany} from '~/js/particles/chengeCompany';
+import {ancorsInit} from '~/js/particles/ancors';
+import {initAcquainted} from '~/js/particles/acquainted';
+import {getActiveCompanyByGetParameter} from '~/js/particles/getActiveCompanyByGetParameter';
+import {initFeatureParallax, initFeatureSlider} from '~/js/particles/features';
+import {initPromoSlider} from '~/js/particles/promos';
+import {initVideoPlayers} from '~/js/particles/promos';
 import '~/js/particles/features';
 import '~/js/particles/promos';
 
 window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
 };
 
 window.addEventListener('DOMContentLoaded', () => {
-  initPreloader();
-  initMainForm();
-  ancorsInit();
-  initAcquainted();
-  chengeCompany(getActiveCompanyByGetParameter(), true);
-  initFeatureSlider();
-  initPromoSlider();
-  initVideoPlayers();
+    initPreloader();
+    initMainForm();
+    ancorsInit();
+    initAcquainted();
+    chengeCompany(getActiveCompanyByGetParameter(), true);
+    initFeatureSlider();
+    initFeatureParallax();
+    initPromoSlider();
+    initVideoPlayers();
 });
--- src/js/particles/features.js
+++ src/js/particles/features.js
@@ -2,80 +2,104 @@ import Flickity from 'flickity';
 import 'flickity/dist/flickity.min.css';
 
 export const initFeatureSlider = () => {
-  const _sliderFeatures = document.querySelector('.slider-features');
-  const _prevItem = document.querySelector('.slider-features__button_prev');
-  const _nextItem = document.querySelector('.slider-features__button_next');
-  const _pageDots = document.querySelector('.page-dots');
+    const _sliderFeatures = document.querySelector('.slider-features');
+    const _prevItem = document.querySelector('.slider-features__button_prev');
+    const _nextItem = document.querySelector('.slider-features__button_next');
+    const _pageDots = document.querySelector('.page-dots');
 
-  const imgs = [];
-  // const imgs = _sliderFeatures.querySelectorAll('.figures');
-  const _figuresAll = _sliderFeatures.querySelectorAll('.figures');
-  _figuresAll.forEach(figuresSlide => {
-    const slideFigures = figuresSlide.querySelectorAll('.slide-figure');
-    imgs.push(slideFigures);
-  });
-
-  const docStyle = document.documentElement.style;
-  const transformProp =
-    typeof docStyle.transform == 'string' ? 'transform' : 'WebkitTransform';
-
-  const dotsArray = [];
-  let activeDot = null;
-
-  const featuresSlider = new Flickity(_sliderFeatures, {
-    cellAlign: 'center',
-    percentPosition: false,
-    prevNextButtons: false,
-    wrapAround: false,
-    pageDots: false,
-    on: {
-      scroll: function (progress) {
-        const carousel = this;
-        const carouselWidth = carousel.size.width;
-
-        carousel.slides.forEach(function (slide, i) {
-          const slideFigures = imgs[i];
-          const x = (((slide.target + carousel.x) % carouselWidth) * -1) / 6;
-
-          slideFigures.forEach(figure => {
-            if (
-              figure.offsetLeft + x < carouselWidth &&
-              figure.offsetLeft + x > carouselWidth * 0.14
-            )
-              figure.style[transformProp] = 'translateX(' + x + 'px)';
-          });
-        });
-      },
-      change: function (index) {
-        activeDot && activeDot.classList.remove('page-dots__dot_active');
-        activeDot = dotsArray[index];
-        activeDot.classList.add('page-dots__dot_active');
-      }
-    }
-  });
-
-  for (let i = 0; i < featuresSlider.slides.length; i++) {
-    const dotElement = document.createElement('li');
-
-    dotElement.addEventListener('click', () => {
-      featuresSlider.select(i);
+    const imgs = [];
+    // const imgs = _sliderFeatures.querySelectorAll('.figures');
+    const _figuresAll = _sliderFeatures.querySelectorAll('.figures');
+    _figuresAll.forEach(figuresSlide => {
+        const slideFigures = figuresSlide.querySelectorAll('.slide-figure');
+        imgs.push(slideFigures);
     });
 
-    if (i == 0) {
-      dotElement.classList.add('page-dots__dot');
-      dotElement.classList.add('page-dots__dot_active');
-      activeDot = dotElement;
-    } else dotElement.classList.add('page-dots__dot');
+    const docStyle = document.documentElement.style;
+    const transformProp =
+        typeof docStyle.transform == 'string' ? 'transform' : 'WebkitTransform';
 
-    _pageDots.appendChild(dotElement);
-    dotsArray.push(dotElement);
-  }
+    const dotsArray = [];
+    let activeDot = null;
 
-  _prevItem.addEventListener('click', () => {
-    featuresSlider.next();
-  });
+    const featuresSlider = new Flickity(_sliderFeatures, {
+        cellAlign: 'center',
+        percentPosition: false,
+        prevNextButtons: false,
+        wrapAround: false,
+        pageDots: false,
+        on: {
+            scroll: function (progress) {
+                const carousel = this;
+                const carouselWidth = carousel.size.width;
 
-  _nextItem.addEventListener('click', () => {
-    featuresSlider.previous();
-  });
+                carousel.slides.forEach(function (slide, i) {
+                    const slideFigures = imgs[i];
+                    const x = (((slide.target + carousel.x) % carouselWidth) * -1) / 6;
+
+                    slideFigures.forEach(figure => {
+                        if (
+                            figure.offsetLeft + x < carouselWidth &&
+                            figure.offsetLeft + x > carouselWidth * 0.14
+                        )
+                            figure.style[transformProp] = 'translateX(' + x + 'px)';
+                    });
+                });
+            },
+            change: function (index) {
+                activeDot && activeDot.classList.remove('page-dots__dot_active');
+                activeDot = dotsArray[index];
+                activeDot.classList.add('page-dots__dot_active');
+            }
+        }
+    });
+
+    for (let i = 0; i < featuresSlider.slides.length; i++) {
+        const dotElement = document.createElement('li');
+
+        dotElement.addEventListener('click', () => {
+            featuresSlider.select(i);
+        });
+
+        if (i == 0) {
+            dotElement.classList.add('page-dots__dot');
+            dotElement.classList.add('page-dots__dot_active');
+            activeDot = dotElement;
+        } else dotElement.classList.add('page-dots__dot');
+
+        _pageDots.appendChild(dotElement);
+        dotsArray.push(dotElement);
+    }
+
+    _prevItem.addEventListener('click', () => {
+        featuresSlider.next();
+    });
+
+    _nextItem.addEventListener('click', () => {
+        featuresSlider.previous();
+    });
 };
+
+const _featuresFigures = document.querySelectorAll('.features-grid-figure');
+let windowHeight = document.documentElement.clientHeight;
+
+window.addEventListener('resize', () => {
+    windowHeight = document.documentElement.clientHeight;
+});
+
+export const initFeatureParallax = () => {
+    window.addEventListener('scroll', setFeatureParallaxPosition);
+};
+
+function setFeatureParallaxPosition() {
+    const speedArray = [73, 54, 83, 61, 65, 70];
+    _featuresFigures.forEach((featuresFigure, i) => {
+        if (
+            Math.abs(featuresFigure.getBoundingClientRect().top) <= windowHeight
+        ) {
+            const offset =
+                (featuresFigure.getBoundingClientRect().top * speedArray[i]) / windowHeight;
+            featuresFigure.style.transform = `translateY(${offset}px)`;
+        }
+    })
+}
