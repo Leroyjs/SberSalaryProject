67e1cf33a8144317cfc36e33cb72a1cbcaf397c3
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-06-13 15:43:45 +0500

    Add promo slider

--- src/js/entry/main.js
+++ src/js/entry/main.js
@@ -1,7 +1,14 @@
 import '~/sass/main';
-import { initPreloader } from '~/js/particles/preloader';
+import {initPreloader} from '~/js/particles/preloader';
+import {initFeatureSlider} from '~/js/particles/features';
+import {initPromoSlider} from '~/js/particles/promos';
+import {initVideoPlayers} from '~/js/particles/promos';
 import '~/js/particles/features';
+import '~/js/particles/promos';
 
 window.addEventListener('DOMContentLoaded', () => {
-  initPreloader();
+    initPreloader();
+    initFeatureSlider();
+    initPromoSlider();
+    initVideoPlayers();
 });
--- src/js/particles/features.js
+++ src/js/particles/features.js
@@ -1,70 +1,71 @@
 import Flickity from 'flickity';
 import 'flickity/dist/flickity.min.css';
 
-const _sliderFeatures = document.querySelector('.slider-features');
-const _prevItem = document.querySelector('.slider-features__button_prev');
-const _nextItem = document.querySelector('.slider-features__button_next');
-const _pageDots = document.querySelector('.page-dots');
+export const initFeatureSlider = () => {
+    const _sliderFeatures = document.querySelector('.slider-features');
+    const _prevItem = document.querySelector('.slider-features__button_prev');
+    const _nextItem = document.querySelector('.slider-features__button_next');
+    const _pageDots = document.querySelector('.page-dots');
 
-const imgs = _sliderFeatures.querySelectorAll('.figures');
-const docStyle = document.documentElement.style;
-const transformProp = typeof docStyle.transform == 'string' ?
-    'transform' : 'WebkitTransform';
+    const imgs = _sliderFeatures.querySelectorAll('.figures');
+    const docStyle = document.documentElement.style;
+    const transformProp = typeof docStyle.transform == 'string' ?
+        'transform' : 'WebkitTransform';
 
-const dotsArray = [];
-let activeDot = null;
+    const dotsArray = [];
+    let activeDot = null;
 
-const featuresSlider = new Flickity(_sliderFeatures, {
-    cellAlign: 'center',
-    percentPosition: false,
-    prevNextButtons: false,
-    wrapAround: false,
-    pageDots: false,
-    on: {
-        scroll: function () {
-            const carousel = this;
-            const carouselWidth = carousel.size.width;
+    const featuresSlider = new Flickity(_sliderFeatures, {
+        cellAlign: 'center',
+        percentPosition: false,
+        prevNextButtons: false,
+        wrapAround: false,
+        pageDots: false,
+        on: {
+            scroll: function () {
+                const carousel = this;
+                const carouselWidth = carousel.size.width;
 
-            carousel.slides.forEach(function (slide, i) {
-                const img = imgs[i];
-                const x = (slide.target + carousel.x) % carouselWidth * -1 / 5;
-                img.style[transformProp] = 'translateX(' + x + 'px)';
-            });
+                carousel.slides.forEach(function (slide, i) {
+                    const img = imgs[i];
+                    const x = (slide.target + carousel.x) % carouselWidth * -1 / 5;
+                    img.style[transformProp] = 'translateX(' + x + 'px)';
+                });
+            },
+            change: function (index) {
+                activeDot && activeDot.classList.remove('page-dots__dot_active');
+                activeDot = dotsArray[index];
+                activeDot.classList.add('page-dots__dot_active');
+            }
         },
-        change: function (index) {
-            activeDot && activeDot.classList.remove('page-dots__dot_active');
-            activeDot = dotsArray[index];
-            activeDot.classList.add('page-dots__dot_active');
-        }
-    },
-});
-
-
-for (let i = 0; i < featuresSlider.slides.length; i++) {
-    const dotElement = document.createElement('li');
-
-    dotElement.addEventListener('click', () => {
-        featuresSlider.select(i);
     });
 
-    if (i == 0) {
-        dotElement.classList.add('page-dots__dot');
-        dotElement.classList.add('page-dots__dot_active');
-        activeDot = dotElement;
-    } else
-        dotElement.classList.add('page-dots__dot');
 
-    _pageDots.appendChild(dotElement);
-    dotsArray.push(dotElement);
+    for (let i = 0; i < featuresSlider.slides.length; i++) {
+        const dotElement = document.createElement('li');
+
+        dotElement.addEventListener('click', () => {
+            featuresSlider.select(i);
+        });
+
+        if (i == 0) {
+            dotElement.classList.add('page-dots__dot');
+            dotElement.classList.add('page-dots__dot_active');
+            activeDot = dotElement;
+        } else
+            dotElement.classList.add('page-dots__dot');
+
+        _pageDots.appendChild(dotElement);
+        dotsArray.push(dotElement);
+    }
+
+    _prevItem.addEventListener('click', () => {
+        featuresSlider.next();
+    });
+
+
+    _nextItem.addEventListener('click', () => {
+        featuresSlider.previous();
+    })
 }
-
-_prevItem.addEventListener('click', () => {
-    featuresSlider.next();
-});
-
-
-_nextItem.addEventListener('click', () => {
-    featuresSlider.previous();
-})
-
 
