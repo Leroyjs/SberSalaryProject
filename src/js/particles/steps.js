26c6066233047c497e4f3993dca2e788223d73bc
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-06-16 01:23:58 +0500

    fix vertical paralax slider

--- src/js/particles/steps.js
+++ src/js/particles/steps.js
@@ -1,13 +1,6 @@
-const _figuresArray = document.querySelectorAll('.figures');
-const _figuresAll = [];
-_figuresArray.forEach(group => {
-  _figuresAll.push(group.querySelectorAll('.slide-figure'));
-});
-
-_figuresAll.forEach((figures, i) => {
-  figures.forEach((featuresFigure, j) => {
-    featuresFigure.style.transform = `translateY(25px)`;
-  });
+const figures = document.querySelectorAll('.figures');
+figures.forEach((figure) => {
+    figure.style.transform = `translateY(-45px)`;
 });
 
 let windowHeight = document.documentElement.clientHeight;
@@ -17,19 +10,15 @@ let windowHeight = document.documentElement.clientHeight;
 // });
 
 function setFeatureParallaxPosition() {
-  const speedArray = [28, 26, 24, 29, 27, 27];
-
-  _figuresAll.forEach((figures, i) => {
-    figures.forEach((featuresFigure, j) => {
-      const figureTop = featuresFigure.getBoundingClientRect().top;
-      if (Math.abs(figureTop) <= windowHeight) {
-        const offset = (figureTop * speedArray[i]) / windowHeight;
-        featuresFigure.style.transform = `translateY(${offset}px)`;
-      }
+    figures.forEach((figure, i) => {
+        const figureTop = figure.getBoundingClientRect().top;
+        if (Math.abs(figureTop) <= windowHeight) {
+            const offset = (figureTop * -80) / windowHeight;
+            figure.style.transform = `translateY(${offset}px)`;
+        }
     });
-  });
 }
 
 export const initSliderFiguresParallax = () => {
-  window.addEventListener('scroll', setFeatureParallaxPosition);
+    window.addEventListener('scroll', setFeatureParallaxPosition);
 };
