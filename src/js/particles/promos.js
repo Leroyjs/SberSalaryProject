acaf70743a41a181933c7be91aa7963b121ffa11
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-06-30 18:26:38 +0500

    Fix resize promo video

--- src/js/particles/promos.js
+++ src/js/particles/promos.js
@@ -3,107 +3,125 @@ import Flickity from 'flickity';
 import 'flickity/dist/flickity.min.css';
 
 export const initVideoPlayers = () => {
-  const players = document.querySelectorAll('.player');
+    const players = document.querySelectorAll('.player');
 
-  players.forEach(item => {
-    initVideo(item);
-  });
-
-  function initVideo(item) {
-    const videoId = item.getAttribute('data-video-id');
-    const playerCover = item.firstChild.nextElementSibling;
-    playerCover.style.backgroundImage = `url('https://img.youtube.com/vi/${videoId}/maxresdefault.jpg')`;
-
-    item.addEventListener('click', () => {
-      let player;
-
-      let firstScriptTag = document.getElementsByTagName('script')[0];
-      let secScriptTag = document.getElementsByTagName('script')[1];
-
-      const isYT =
-        firstScriptTag.getAttribute('src') ===
-          'https://www.youtube.com/iframe_api' ||
-        secScriptTag.getAttribute('src') ===
-          'https://www.youtube.com/iframe_api';
-
-      if (!isYT) {
-        let tag = document.createElement('script');
-        tag.src = 'https://www.youtube.com/iframe_api';
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        createNewPlayer();
-      } else {
-        createNewPlayer();
-      }
-
-      function createNewPlayer() {
-        player = new YT.Player(item, {
-          height: '100%',
-          width: '100%',
-          videoId,
-          events: {
-            onReady: onPlayerReady
-          }
-        });
-      }
+    players.forEach(item => {
+        initVideo(item);
     });
 
-    function onPlayerReady(event) {
-      event.target.playVideo();
+    function initVideo(item) {
+        const videoId = item.getAttribute('data-video-id');
+        const playerCover = item.firstChild.nextElementSibling;
+        playerCover.style.backgroundImage = `url('https://img.youtube.com/vi/${videoId}/maxresdefault.jpg')`;
+
+        item.addEventListener('click', () => {
+            let player;
+
+            let firstScriptTag = document.getElementsByTagName('script')[0];
+            let secScriptTag = document.getElementsByTagName('script')[1];
+
+            const isYT =
+                firstScriptTag.getAttribute('src') ===
+                'https://www.youtube.com/iframe_api' ||
+                secScriptTag.getAttribute('src') ===
+                'https://www.youtube.com/iframe_api';
+
+            if (!isYT) {
+                let tag = document.createElement('script');
+                tag.src = 'https://www.youtube.com/iframe_api';
+                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+                createNewPlayer();
+            } else {
+                createNewPlayer();
+            }
+
+            function createNewPlayer() {
+                player = new YT.Player(item, {
+                    height: '100%',
+                    width: '100%',
+                    videoId,
+                    events: {
+                        onReady: onPlayerReady
+                    }
+                });
+            }
+        });
+
+        function onPlayerReady(event) {
+            event.target.playVideo();
+        }
     }
-  }
 };
 
 export const initPromoSlider = () => {
-  const _sliderPromos = document.querySelector('.slider-promos');
-  const _sliderControls = document.querySelector('.promos__controls');
-  const _prevItem = _sliderControls.querySelector(
-    '.slider-promos__button_prev'
-  );
-  const _nextItem = _sliderControls.querySelector(
-    '.slider-promos__button_next'
-  );
-  const _pageDots = _sliderControls.querySelector('.page-dots');
+    const _sliderPromos = document.getElementById('promo-slider');
+    const _sliderControls = document.querySelector('.promos__controls');
 
-  const dotsArray = [];
-  let activeDot = null;
+    const _prevItem = _sliderControls.querySelector(
+        '.slider-promos__button_prev'
+    );
+    const _nextItem = _sliderControls.querySelector(
+        '.slider-promos__button_next'
+    );
+    const _pageDots = _sliderControls.querySelector('.page-dots');
+    const mql_720 = window.matchMedia("screen and (min-width: 720px)");
 
-  const promoSlider = new Flickity(_sliderPromos, {
-    cellAlign: 'center',
-    percentPosition: false,
-    prevNextButtons: false,
-    wrapAround: false,
-    pageDots: false,
-    on: {
-      change: function (index) {
-        activeDot && activeDot.classList.remove('page-dots__dot_active');
-        activeDot = dotsArray[index];
-        activeDot.classList.add('page-dots__dot_active');
-      }
+    function initFlickity(slider) {
+        const dotsArray = [];
+        let activeDot = null;
+        const sliderOptions = {
+            cellAlign: 'center',
+            percentPosition: false,
+            prevNextButtons: false,
+            wrapAround: false,
+            pageDots: false,
+            on: {
+                change: function (index) {
+                    activeDot && activeDot.classList.remove('page-dots__dot_active');
+                    activeDot = dotsArray[index];
+                    activeDot.classList.add('page-dots__dot_active');
+                }
+            }
+        }
+
+        slider = new Flickity(_sliderPromos, sliderOptions);
+
+        for (let i = 0; i < slider.slides.length; i++) {
+            const dotElement = document.createElement('li');
+
+            dotElement.addEventListener('click', () => {
+                promoSlider.select(i);
+            });
+
+            if (i == 0) {
+                dotElement.classList.add('page-dots__dot');
+                dotElement.classList.add('page-dots__dot_active');
+                activeDot = dotElement;
+            } else dotElement.classList.add('page-dots__dot');
+
+            _pageDots.appendChild(dotElement);
+            dotsArray.push(dotElement);
+        }
+
+        _prevItem.addEventListener('click', () => {
+            promoSlider.next();
+        });
+
+        _nextItem.addEventListener('click', () => {
+            promoSlider.previous();
+        });
     }
-  });
 
-  for (let i = 0; i < promoSlider.slides.length; i++) {
-    const dotElement = document.createElement('li');
+    if (!mql_720.matches) {
+        initFlickity(_sliderPromos);
+    }
 
-    dotElement.addEventListener('click', () => {
-      promoSlider.select(i);
+    // replaceChild
+    mql_720.addEventListener('change', (e) => {
+        if (e.matches) {
+            // promoSlider.destroy();
+        } else {
+            initFlickity(_sliderPromos);
+        }
     });
-
-    if (i == 0) {
-      dotElement.classList.add('page-dots__dot');
-      dotElement.classList.add('page-dots__dot_active');
-      activeDot = dotElement;
-    } else dotElement.classList.add('page-dots__dot');
-
-    _pageDots.appendChild(dotElement);
-    dotsArray.push(dotElement);
-  }
-
-  _prevItem.addEventListener('click', () => {
-    promoSlider.next();
-  });
-
-  _nextItem.addEventListener('click', () => {
-    promoSlider.previous();
-  });
 };
